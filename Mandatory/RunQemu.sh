e484cf3b21c734d455beb8d4c2c0011abf8a2834
w1spo <w16spo@gmail.com>
2025-10-26 01:27:32 +0000

    Another Commit

---
+++ Mandatory/RunQemu.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+qemu-system-x86_64 -cdrom system.iso -m 512M
--- build.sh
+++ build.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-set -e  # zatrzymaj skrypt przy błędzie
+set -e
 
 rm -rf Build
 mkdir -p Build/Compile
 
-# Kompilacja asemblera
-i686-elf-as boot.s -o Build/Compile/boot.o
+# Compile Bootloader
+i686-elf-as Mandatory/boot.s -o Build/Compile/boot.o
 
-# Kompilacja C
+# Compile Kernel
 i686-elf-gcc -c Kernel/src/kernel.c -o Build/Compile/kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra -nostdlib -nostdinc
 
-# Linkowanie
-i686-elf-gcc -T linker.ld -o Build/kernel.bin -ffreestanding -O2 -nostdlib Build/Compile/boot.o Build/Compile/kernel.o
+# Link
+i686-elf-gcc -T Mandatory/linker.ld -o Build/kernel.bin -ffreestanding -O2 -nostdlib Build/Compile/boot.o Build/Compile/kernel.o
 
-# Sprawdzenie multiboot
+# Multiboot GRUB check
 if grub-file --is-x86-multiboot Build/kernel.bin; then
     echo "Multiboot confirmed"
 else
@@ -21,12 +21,13 @@ else
     exit 1
 fi
 
-# Tworzenie struktury katalogów
+# Create Directories
 mkdir -p Build/isodir/boot/grub
 cp Build/kernel.bin Build/isodir/boot/kernel.bin
-cp grub.cfg Build/isodir/boot/grub/grub.cfg
+cp Mandatory/grub.cfg Build/isodir/boot/grub/grub.cfg
+cp Mandatory/RunQemu.sh Build/
 
-# Tworzenie obrazu ISO
+# Create ISO image
 grub-mkrescue -o Build/system.iso Build/isodir
 
 echo "✅ Build completed: Build/system.iso"
