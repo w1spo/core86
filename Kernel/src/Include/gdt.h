eac4d8eb30792f67db526f6cd4cc91ca66aeb806
w1spo <w16spo@gmail.com>
2026-01-28 21:17:55 +0000

    Added entry for GDT

---
+++ Kernel/src/Include/gdt.h
@@ -0,0 +1,16 @@
+#ifndef GDT_H
+#define GDT_H
+
+#include "types.h"
+
+struct gdt_entry {
+
+    uint16 limit_low;
+    uint16 base_low;
+    uint8 base_middle;
+    uint8 access;
+    uint8 granularity;
+    uint8 base_high;
+};
+
+#endif
